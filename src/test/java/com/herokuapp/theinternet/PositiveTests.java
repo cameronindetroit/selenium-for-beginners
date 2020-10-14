3f9616c43d15820a8d599efe111f09fa30130a54
Cameron Barnes <cameron.barnes@perficient.com>
2020-10-14 11:50:37 -0400

    Positive test class

---
+++ src/test/java/com/herokuapp/theinternet/PositiveTests.java
@@ -0,0 +1,28 @@
+package com.herokuapp.theinternet;
+
+public class PositiveTests {
+	
+	public void logintest() {
+		
+		//create driver 
+		
+		//open test page
+		
+		//Enter user name
+		
+		//Enter password 
+		
+		//Click login button
+		
+		//verifications: 
+		
+		//URL is /secure 
+		
+		//successful login message
+		
+		//Logout button is visible. 
+		
+		
+	}
+
+}
