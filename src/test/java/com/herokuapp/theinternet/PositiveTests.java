a98ccd1e82997409cefa268cdbd58359dfc979f2
Cameron Barnes <cameron.barnes@perficient.com>
2020-10-14 13:25:02 -0400

    Open browser, navigate to page

--- src/test/java/com/herokuapp/theinternet/PositiveTests.java
+++ src/test/java/com/herokuapp/theinternet/PositiveTests.java
@@ -1,12 +1,34 @@
 package com.herokuapp.theinternet;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
 public class PositiveTests {
 	
+	@Test
 	public void logintest() {
 		
+		System.out.println("Starting login test");
+		
 		//create driver 
+		System.setProperty("webdriver.chrome.driver", "src\\main\\resources\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+		//Sleep for 3 seconds
+				sleep(3000);
+				
+				//Maximize browser window
+				driver.manage().window().maximize();
+				
 		
 		//open test page
+		String url = "http://the-internet.herokuapp.com/login";
+		driver.get(url);
+		System.out.println("The page is open");
+		
+		//Sleep for 3 seconds
+		sleep(2000);
 		
 		//Enter user name
 		
@@ -22,7 +44,19 @@ public class PositiveTests {
 		
 		//Logout button is visible. 
 		
+		//Close Browser
+		driver.quit();
 		
+		
+	}
+
+	private void sleep(long m) {
+		try {
+			Thread.sleep(m);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
